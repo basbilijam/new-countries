a9865b659a9ef5fd9a583bf53cf3a3b82f0c0cce
basbilijam <basbilijam@gmail.com>
2017-01-30 17:42:42 +0100

    Initial commit

---
+++ json-file-reader.js
@@ -0,0 +1,10 @@
+var fs = require('fs');
+
+module.exports = {
+ 
+  readFile: function(path, callback) {
+    fs.readFile(path, function(error, data) {
+      return callback(error, JSON.parse(data));
+    });
+  }
+};
